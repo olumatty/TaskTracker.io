4011c91562c52ac95332d072ba09bf67a96d59e3
Matthew Adebayo <olumatty@gmail.com>
2024-08-13 01:06:14 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import AddTask from "./Components/AddTask";
+import { useState } from "react";
 
 function App() {
+  const {taskList, setTaskList} = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="text-2xl font-bold py-4 pl-6">The Task Tracker</h1>
+      <p className="text-xl pl-6">Hi there!!</p>
+      <div className="flex flex-row items-center">
+        <p className="text-xl pl-6"> Click</p>
+        <AddTask taskList ={taskList} setTaskList={setTaskList}/>
+        <p className="text-xl ml-2">to add task</p>
+      </div>
+    </>
   );
 }
 
